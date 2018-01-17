2510bbec7bc0a5109de2a345f6213368ef1286b6
Lauryn McFarland <lauryn@lauryns-mbp.attlocal.net>
2018-01-17 09:40:34 -0600

    edited liri file

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+console.log(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
